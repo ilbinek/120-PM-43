0b3a5b7caa02c533b8c9721492ffda30639963b0
Tklama <martin.sebensky@gmail.com>
2024-03-24 11:20:34 +0100

    Added animations, textures, etc

--- addons/pm_43/script_component.hpp
+++ addons/pm_43/script_component.hpp
@@ -18,3 +18,21 @@
 //#define TBD_MORT(cmp,fncName) tbd##_##cmp##_##fnc##_##fncName
 
 #define TBD_120mm_PM_43 tbd_120mm_pm_43
+
+#define MORTFUNC(var1) tbd_mortars_main_fnc_##var1
+#define QMORTFUNC(var1) QUOTE(MORTFUNC(var1))
+#define MORTPATH(var1,var2) x\tbd_mortars\addons\var1\var2
+#define QMORTPATH(var1,var2) QUOTE(MORTPATH(var1,var2))
+
+#define TBD_2B25_GALL tbd_2b25_gall
+#define TBD_2B25_WEAPON tbd_2b25_weapon
+#define TBD_2B25_BOX_HE tbd_2b25_box_he
+#define TBD_2B25_HE tbd_2b25_shell_he
+#define TBD_2B25_82mm_SHELL_AMMO_HE tbd_2B25_82mm_shell_ammo_he
+
+#define TBD_2B25_CARRY tbd_2b25_carry
+#define TBD_2B25_TRIPOD tbd_2b25_tripod
+#define TBD_2B25_BASEPLATE tbd_2b25_baseplate
+
+#define TBD_2B25_SHOT_SOUNDSET tbd_2b25_shot_soundset
+#define TBD_2B25_SHOT_SOUNDSHADER tbd_2b25_shot_soundshader
--- addons/pm_43/tbd_120mm_pm_43/config.cpp
+++ addons/pm_43/tbd_120mm_pm_43/config.cpp
@@ -1,7 +1,17 @@
 #include "..\script_component.hpp"
 
 class CfgPatches {
-    
+    class TBD_120MM_PM_43 {
+        addonRootClass = "A3_Armor_F_Beta";
+        requiredAddons[] = {"cba_main", "ace_interact_menu"};
+        requiredVersion = 0.5;
+        weapons[] = {QUOTE(TBD_2B25_WEAPON)};
+        units[] = {QUOTE(TBD_120MM_PM_43)};
+    };
 };
 
+class Mode_SemiAuto;
+#include "CfgSoundShaders.hpp"
+#include "CfgSoundSets.hpp"
+#include "CfgWeapons.hpp"
 #include "CfgVehicles.hpp"
