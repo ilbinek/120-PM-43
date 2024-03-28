c6e88d696562bb67c24a5b8ba5d7f007806e0562
Sotirios Pupakis <sotirek@gmail.com>
2024-03-28 19:51:29 +0100

    texture reference fix, version bump, fixed tvoji mamu

--- mod.cpp
+++ mod.cpp
@@ -1,8 +1,8 @@
-name			= "120-PM-43-2B11";						// Name of your mod
+name			= "120-PM-43";						// Name of your mod
 author          = "TBD Team";
 actionName		= "Author";										// Text displayed on the "website" button
 action			= "https://tbdevent.eu/";		// Website URL, that can accessed from the expansions menu
 tooltipOwned	= "TBD Event";			                // Tool tip displayed when the mouse is left over, in the main menu
 
 // Overview text, displayed from the extension menu
-overview	= "120-PM-43-2B11";
+overview	= "120-PM-43";
